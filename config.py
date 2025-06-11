584beeb72d3d92b1b348ab181ee429f9837976e9
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2025-06-11 10:19:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "6231550362"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7998441787"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
